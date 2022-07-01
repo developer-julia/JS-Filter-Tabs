ec8149ea065e2225745e3d026a6df6ad6862b1b4
developer-julia <developer_julia@mail.ru>
2022-07-01 16:25:59 +0400

    разработка фильтра и доп. стили

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,25 @@
-$(function () {
+function app() {
+    const buttons = document.querySelectorAll('.films-tabs__link')
+    const cards = document.querySelectorAll('.article-item')
 
-})
+    function filter(category, items) {
+        items.forEach((item) => {
+            const isItemFiltered = !item.classList.contains(category)
+            const isShowAll = category.toLowerCase() === 'all'
+            if (isItemFiltered && !isShowAll) {
+                item.classList.add('hide')
+            } else {
+                item.classList.remove('hide')
+            }
+        })
+    }
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const currentCategory = button.dataset.filter
+            filter(currentCategory, cards)
+        })
+    })
+}
+
+app()
